b0f5dde6a41daa407dfa23e8df828bdbc3624947
Alejandro Barros Cuetos <jandrob1978@gmail.com>
2014-07-31 00:01:10 +0200

    Initial Commit

---
+++ FCComponents/FCComponents.h
@@ -0,0 +1,19 @@
+//
+//  FCComponents.h
+//  FCComponents
+//
+//  Created by Alejandro Barros Cuetos on 31/07/14.
+//  Copyright (c) 2014 Alejandro Barros Cuetos. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FCComponents.
+FOUNDATION_EXPORT double FCComponentsVersionNumber;
+
+//! Project version string for FCComponents.
+FOUNDATION_EXPORT const unsigned char FCComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FCComponents/PublicHeader.h>
+
+
